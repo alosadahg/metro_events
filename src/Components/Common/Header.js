62df722a6aa4e2a47073e0d0e17a4324bd4f9fa5
Lucky Jones <luckyjonesbuayan@gmail.com>
2024-03-23 11:35:45 +0800

    minor style tweaks

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { faBell, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import HeaderNotification from "../Main/Notification/HeaderNotification";
 
 const Header = () => {
+  const [isNotifClicked, setIsNotifClicked] = useState(false);
   return (
     <nav className="Header">
       <h2>MetroEvents</h2>
       <ul>
         <li>Welcome, Tanga!</li>
         <li className="notif">
-          <FontAwesomeIcon icon={faBell} />
-
-          <HeaderNotification />
+          <FontAwesomeIcon
+            icon={faBell}
+            className="icon"
+            onClick={() => setIsNotifClicked(!isNotifClicked)}
+          />
+          {isNotifClicked && <HeaderNotification />} {/*the notification*/}
         </li>
         <li>
           <FontAwesomeIcon icon={faUserCircle} />
