49c913d3fd4236443b5e9163cbb00d0d1e06ccd2
Lucky Jones <luckyjonesbuayan@gmail.com>
2024-03-24 15:49:11 +0800

    allowed login and signup to route to a page without credentials for testing purposes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,74 +1,82 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 
 const defaultTheme = createTheme();
 
 const Login = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!email || !password) {
-      setError('Please enter both email and password');
+      setError("Please enter both email and password");
       return;
     }
 
     try {
       const response = await fetch(`http://localhost:8000/user?email=${email}`);
       if (!response.ok) {
-        throw new Error('Failed to fetch user information');
+        throw new Error("Failed to fetch user information");
       }
       const userData = await response.json();
       if (!userData || userData.length === 0 || !userData[0].id) {
-        throw new Error('User not found or missing user ID');
+        throw new Error("User not found or missing user ID");
       }
       const userId = userData[0].id;
-      console.log('User ID:', userId);
+      console.log("User ID:", userId);
 
       navigate(`/dashboard/${userId}`);
-
     } catch (err) {
-      console.error('Error fetching user information:', err.message);
-      setError('Failed to fetch user information. Please try again.');
+      console.error("Error fetching user information:", err.message);
+      setError("Failed to fetch user information. Please try again.");
     }
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+      <Grid
+        container
+        component="main"
+        sx={{ height: "100vh", justifyContent: "center", alignItems: "center" }}
+      >
         <CssBaseline />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Login
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -93,15 +101,22 @@ const Login = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              {error && <Typography variant="body2" color="error">{error}</Typography>}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
+              {error && (
+                <Typography variant="body2" color="error">
+                  {error}
+                </Typography>
+              )}
+              <Link to={"/event-discovery"}>
+                {/* temporary go to event discovery for testing purposes */}
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+              </Link>
               <Grid container>
                 <Grid item>
                   <Link to="/register" variant="body2">
@@ -115,6 +130,6 @@ const Login = () => {
       </Grid>
     </ThemeProvider>
   );
-}
+};
 
 export default Login;
--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -1,83 +1,100 @@
-import React, { useState, useEffect } from 'react';
-import { Avatar, Button, CssBaseline, TextField, Grid, Box, Typography, Container, Paper } from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  Paper,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Link, useNavigate } from "react-router-dom";
 
 const defaultTheme = createTheme();
 
 const Registration = () => {
-  const [errorMessage, setErrorMessage] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    user_type: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    user_type: "",
   });
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    setErrorMessage('');
-    setSuccessMessage('');
+    setErrorMessage("");
+    setSuccessMessage("");
     setFormData({
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      user_type: 'user',
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      user_type: "user",
     });
   }, []);
 
   const handleChange = (event) => {
     setFormData({
       ...formData,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     const { firstName, lastName, email, password, user_type } = formData;
-  
+
     if (!firstName || !lastName || !password) {
-      setErrorMessage('Please fill in all fields.');
+      setErrorMessage("Please fill in all fields.");
       return;
     }
-  
+
     if (!validateEmail(email)) {
-      setErrorMessage('Please enter a valid email address.');
+      setErrorMessage("Please enter a valid email address.");
       return;
     }
-  
+
     try {
-      const checkEmailResponse = await fetch(`http://localhost:8000/user?email=${email}`);
+      const checkEmailResponse = await fetch(
+        `http://localhost:8000/user?email=${email}`
+      );
       const existingUserData = await checkEmailResponse.json();
       if (existingUserData && existingUserData.length > 0) {
-        setErrorMessage('Email is already registered.');
+        setErrorMessage("Email is already registered.");
         return;
       }
-  
-      const response = await fetch('http://localhost:8000/user', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ firstName, lastName, email, password, user_type }),
+
+      const response = await fetch("http://localhost:8000/user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          password,
+          user_type,
+        }),
       });
-  
+
       if (!response.ok) {
-        throw new Error('Failed to register. Please try again later.');
+        throw new Error("Failed to register. Please try again later.");
       } else {
-        setSuccessMessage('Registered successfully. Redirecting to login...');
-        navigate('/login');
+        setSuccessMessage("Registered successfully. Redirecting to login...");
+        navigate("/login");
       }
     } catch (error) {
       setErrorMessage(error.message);
     }
   };
-  
 
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -86,37 +103,108 @@ const Registration = () => {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+      <Grid
+        container
+        component="main"
+        sx={{ height: "100vh", justifyContent: "center", alignItems: "center" }}
+      >
         <CssBaseline />
         <Container component="main" maxWidth="xs">
           <Paper elevation={6}>
-            <Box sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Box
+              sx={{
+                p: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
                 <LockOutlinedIcon />
               </Avatar>
               <Typography component="h1" variant="h5">
                 Sign up
               </Typography>
-              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
+              >
                 <Grid container spacing={2}>
                   <Grid item xs={12} sm={6}>
-                    <TextField autoComplete="given-name" name="firstName" required fullWidth id="firstName" label="First Name" autoFocus value={formData.firstName} onChange={handleChange} />
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                      value={formData.firstName}
+                      onChange={handleChange}
+                    />
                   </Grid>
                   <Grid item xs={12} sm={6}>
-                    <TextField required fullWidth id="lastName" label="Last Name" name="lastName" autoComplete="family-name" value={formData.lastName} onChange={handleChange} />
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="family-name"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                    />
                   </Grid>
                   <Grid item xs={12}>
-                    <TextField required fullWidth id="email" label="Email Address" name="email" autoComplete="email" value={formData.email} onChange={handleChange} />
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
                   </Grid>
                   <Grid item xs={12}>
-                    <TextField required fullWidth name="password" label="Password" type="password" id="password" autoComplete="new-password" value={formData.password} onChange={handleChange} />
+                    <TextField
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                      value={formData.password}
+                      onChange={handleChange}
+                    />
                   </Grid>
                 </Grid>
-                {successMessage && <Typography variant="body2" color="success">{successMessage}</Typography>}
-                {errorMessage && <Typography variant="body2" color="error">{errorMessage}</Typography>}
-                <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-                  Sign Up
-                </Button>
+                {successMessage && (
+                  <Typography variant="body2" color="success">
+                    {successMessage}
+                  </Typography>
+                )}
+                {errorMessage && (
+                  <Typography variant="body2" color="error">
+                    {errorMessage}
+                  </Typography>
+                )}
+                <Link to={"/event-discovery"}>
+                  {/* temporary go to event discovery for testing purposes */}
+
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign Up
+                  </Button>
+                </Link>
                 <Grid container justifyContent="flex-end">
                   <Grid item>
                     <Link to="/login" variant="body2">
