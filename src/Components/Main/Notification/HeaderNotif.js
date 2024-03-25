6ec30b973a44a18e05325ac3e48e4229c46176db
hernah <hgalosada@gmail.com>
2024-03-26 03:40:38 +0800

    modified notification, please update isRead parameter of HeaderNotif

--- src/Components/Main/Notification/HeaderNotif.js
+++ src/Components/Main/Notification/HeaderNotif.js
@@ -2,17 +2,16 @@ import React from "react";
 import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const HeaderNotif = ({ title, description, isRead, status }) => {
-  console.log(status);
+const HeaderNotif = ({ title, isRead, status }) => {
   return (
     <div
       className="HeaderNotif"
-      style={{ display: `${status === "upcoming" ? "visible" : "none"}` }}
+      style={{ display: "visible" }}
     >
       <FontAwesomeIcon className="icon" icon={faCircle} />
       <div className={`content ${!isRead && "unread-notifs"}`}>
         <h4 className="title">{title}</h4>
-        <p>{description}</p>
+        <p>{status}</p>
       </div>
       {isRead && <FontAwesomeIcon className="notif-blue" icon={faCircle} />}
     </div>
